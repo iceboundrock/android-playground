41aa16e250a8df844e5916966197963a252258b7
Ruoshi Li <me@ruoshi.li>
2015-05-12 15:29:40 +0800

    finish Anko demo

--- app/src/main/kotlin/li/ruoshi/playground/AnkoActivity.kt
+++ app/src/main/kotlin/li/ruoshi/playground/AnkoActivity.kt
@@ -1,5 +1,24 @@
 package li.ruoshi.playground
 
+import android.app.Activity
+import android.os.Bundle
+import org.jetbrains.anko.*
+
 /**
  * Created by ruoshili on 5/8/15.
  */
+
+public class AnkoActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        verticalLayout {
+            val name = editText()
+            button("Say Hello") {
+                onClick { toast("Hello, ${name.text}!") }
+            }
+        }
+    }
+
+}
