edfc95fed773ca795f5ed40fb61b144ce016050c
Ruoshi Li <me@ruoshi.li>
2015-12-30 14:03:36 +0800

    适配Kotlin 1.0 beta4

--- app/src/main/kotlin/li/ruoshi/playground/ActivitiesAdapter.kt
+++ app/src/main/kotlin/li/ruoshi/playground/ActivitiesAdapter.kt
@@ -1,7 +1,6 @@
 package li.ruoshi.playground
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
@@ -9,23 +8,21 @@ import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.BaseAdapter
 import android.widget.Button
-import java.util.Arrays
-import java.util.Collections
-import li.ruoshi.playground.R
 
 /**
  * Created by ruoshili on 5/9/15.
  */
 public class ActivitiesAdapter(context: Activity)
 : ArrayAdapter<ActivityInfo>(context, R.layout.activity_item, R.id.lanuch_activity_button) {
+    companion object {
+        val TAG = ActivitiesAdapter::class.java.simpleName
+        val PackageName = "li.ruoshi.playground"
+    }
 
-    val TAG = javaClass<ActivitiesAdapter>().getSimpleName()
-    val PackageName = "li.ruoshi.playground"
     val activity = context
     init {
-        val pkgMgr = context.getPackageManager()
+        val pkgMgr = context.packageManager
         val pkgInfo = pkgMgr.getPackageInfo(PackageName, PackageManager.GET_ACTIVITIES)
 
         val launchIntent = pkgMgr.getLaunchIntentForPackage(PackageName)
@@ -42,9 +39,9 @@ public class ActivitiesAdapter(context: Activity)
         val btn = ret.findViewById(R.id.lanuch_activity_button) as Button
 
         val ai = getItem(position)
-        val btnText = getContext().getResources().getString(ai.labelRes)
+        val btnText = context.resources.getString(ai.labelRes)
         Log.d(TAG, "Button text: $btnText, position: $position")
-        btn.setText(btnText)
+        btn.text = btnText
         val cls = Class.forName (ai.name)
         btn.setOnClickListener { v ->
             val intent = Intent(activity, cls);
